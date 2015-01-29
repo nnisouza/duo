90c478bbd0d63f2d8dbad9c9d7854a7e01a5c5a3
Giovani <giov@nnisouza.com>
2015-01-29 12:18:23 -0200

    removing old php

--- header.php
+++ header.php
@@ -9,14 +9,13 @@
         <meta name="robots" content="noindex, nofollow"> 
         <title><?php echo THEME_NAME; ?></title>   
         <meta name="description" content="Duo - Lyfe Style">
-        <link rel="canonical" href="<?php echo THEME_URL; ?>">
         <meta name="format-detection" content="telephone=no">
-        <link rel="author" type="text/plain" href="<?php echo THEME_URL; ?>humans.txt" />
-        <link rel="shortcut icon" type="image/png" href="<?php echo THEME_URL; ?>assets/ico/favicon.png"/>
-        <link rel="icon" type="image/x-icon" href="<?php echo THEME_URL; ?>assets/ico/favicon.ico"> 
+        <link rel="author" type="text/plain" href="humans.txt" />
+        <link rel="shortcut icon" type="image/png" href="assets/ico/favicon.png"/>
+        <link rel="icon" type="image/x-icon" href="assets/ico/favicon.ico"> 
 
         <!-- Styles -->
-        <link rel="stylesheet" type="text/css" href="<?php echo THEME_URL; ?>assets/css/main.css<?php echo '?ver='.THEME_VERSION; ?>"> 
+        <link rel="stylesheet" type="text/css" href="assets/css/main.css<?php echo '?ver='.THEME_VERSION; ?>"> 
 
         <script src='<?php echo THEME_URL; ?>assets/js/modernizr.js'></script> 
 </head>
@@ -34,15 +33,15 @@
             <div id="logo">
                 <a href="<?php echo THEME_URL; ?>">
                     <h1><?php echo THEME_NAME; ?></h1>
-                    <img src="<?php echo THEME_URL; ?>assets/img/logo.png" alt="<?php echo THEME_NAME; ?>">
+                    <img src="assets/img/logo.png" alt="<?php echo THEME_NAME; ?>">
                 </a>
             </div>
 
             <nav id="menu" class="span9 menu">
                 <ul class="nav inline">
-                    <li class="<?php if(PAGE_NAME=='sobre/index.php') { echo 'active'; }?>" > <a href="<?php echo THEME_URL; ?>sobre">Sobre a Duo LifeStyle</a> </li>
-                    <li class="<?php if(PAGE_NAME=='empreendimentos/index.php') { echo 'active'; }?>" > <a href="<?php echo THEME_URL; ?>empreendimentos">Empreendimentos</a></li>
-                    <li class="<?php if(PAGE_NAME=='contato/index.php') { echo 'active'; }?>" > <a href="<?php echo THEME_URL; ?>contato">Contato</a></li>
+                    <li class="<?php if(PAGE_NAME=='sobre/index.php') { echo 'active'; }?>" > <a href="sobre">Sobre a Duo LifeStyle</a> </li>
+                    <li class="<?php if(PAGE_NAME=='empreendimentos/index.php') { echo 'active'; }?>" > <a href="empreendimentos">Empreendimentos</a></li>
+                    <li class="<?php if(PAGE_NAME=='contato/index.php') { echo 'active'; }?>" > <a href="contato">Contato</a></li>
                 </ul>
             </nav> 
         </div>
@@ -53,7 +52,7 @@
         <div class="Slider">
             <ul>
                 <li>
-                    <img src="<?php echo THEME_URL; ?>assets/img/banner.jpg"/>
+                    <img src="assets/img/banner.jpg"/>
                 </li>
             </ul>
         </div>
